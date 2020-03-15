f3adc0e2b431b9c18cc67f26f511d18a2e77bb73
Alexey Perov <perov-alexey@yandex.ru>
2020-03-15 23:54:52 +0300

    Step 2

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,34 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Promise<Void> promise) {
+
+    Promise<String> dbVerticleDeployment = Promise.promise();
+    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment);
+
+    dbVerticleDeployment.future().compose(id -> {
+
+      Promise<String> httpVerticleDeployment = Promise.promise();
+      vertx.deployVerticle(
+        "io.vertx.starter.HttpServerVerticle",
+      new DeploymentOptions().setInstances(2),
+      httpVerticleDeployment);
+
+      return httpVerticleDeployment.future();
+
+    }).setHandler(ar -> {
+      if (ar.succeeded()) {
+        promise.complete();
+      } else {
+        promise.fail(ar.cause());
+      }
+    });
   }
 
 }
